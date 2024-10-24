e2a84b4da8f729c24f2d20b2a7ef9301ca2b5ca9
Anton200021243 <mortal0011@gmail.com>
2024-10-24 19:10:05 +0300

    User entity

--- src/main/java/App/model/User.java
+++ src/main/java/App/model/User.java
@@ -2,14 +2,27 @@ package App.model;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @EqualsAndHashCode
+@Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "name")
     private String username;
+
+    @Column(name = "age")
     private int age;
+
+    @Column(name = "email")
     private String email;
 
     public User(String username, int age, String email) {
